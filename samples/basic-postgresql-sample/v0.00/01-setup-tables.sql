d8da8143da4565addf70e5907c3dddc1f38dbd0b
Vicente Garcia Zuñiga <51667522+somilygz@users.noreply.github.com>
2021-04-20 16:24:11 -0500

    Update 01-setup-tables.sql

--- samples/basic-postgresql-sample/v0.00/01-setup-tables.sql
+++ samples/basic-postgresql-sample/v0.00/01-setup-tables.sql
@@ -19,3 +19,44 @@ CREATE TABLE locations (
     country_id CHARACTER (2) NOT NULL,
     FOREIGN KEY (country_id) REFERENCES countries (country_id)
 );
+
+-- Table: ctl.esquemas_tablas_databricks
+
+-- DROP TABLE ctl.esquemas_tablas_databricks;
+
+CREATE TABLE IF NOT EXISTS ctl.esquemas_tablas_databricks
+(
+    nombre_tabla character varying(250) COLLATE pg_catalog."default" NOT NULL,
+    ddl character varying COLLATE pg_catalog."default" NOT NULL,
+    columnas_particion character varying COLLATE pg_catalog."default",
+    columnas_zorder character varying COLLATE pg_catalog."default",
+    ruta_wasb_destino character varying COLLATE pg_catalog."default" NOT NULL,
+    separador_columna character varying COLLATE pg_catalog."default" NOT NULL,
+    archivo_contiene_header boolean NOT NULL,
+    codificacion_archivo character varying COLLATE pg_catalog."default" NOT NULL,
+    formato_destino character varying COLLATE pg_catalog."default" NOT NULL,
+    modo_escritura character varying COLLATE pg_catalog."default" NOT NULL,
+    idsubsistemaorigen integer,
+    columnas_particion_calculadas character varying(500) COLLATE pg_catalog."default",
+    nombre_bd_parquet character varying(255) COLLATE pg_catalog."default",
+    aplicar_optimizer boolean
+);
+    ALTER TABLE ctl.esquemas_tablas_databricks
+    OWNER to "Sqladmindwh";
+
+
+-- Table: ctl.log_csvparquet
+
+-- DROP TABLE ctl.log_csvparquet;
+
+CREATE TABLE IF NOT EXISTS ctl.log_csvparquet
+(
+    id integer NOT NULL DEFAULT nextval('ctl.log_csvparquet_id_seq'::regclass),
+    aplicativo character varying(8000) COLLATE pg_catalog."default",
+    insercion text COLLATE pg_catalog."default",
+    nivel character varying(8000) COLLATE pg_catalog."default",
+    mensaje text COLLATE pg_catalog."default",
+    origen text COLLATE pg_catalog."default"
+);
+    ALTER TABLE ctl.log_csvparquet
+    OWNER to "Sqladmindwh";
